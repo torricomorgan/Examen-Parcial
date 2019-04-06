0512d25b3736f1be2fcaf57eea259145af456433
Matias Derek Torrico Morgan <matias_dtm@hotmail.com>
2019-04-06 09:22:30 -0400

    Primer Commit MVC

---
+++ ExamenParcial/MVCFriend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCFriend.Startup))]
+namespace MVCFriend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
