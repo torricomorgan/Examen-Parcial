42a923b01df8e9858e4d3390c061380ebeef96d6
Matias Derek Torrico Morgan <matias_dtm@hotmail.com>
2019-04-06 10:01:48 -0400

    Commit API modelo v2

--- ExamenParcial/APIFriend/App_Start/IdentityConfig.cs
+++ ExamenParcial/APIFriend/App_Start/IdentityConfig.cs
@@ -28,8 +28,8 @@ namespace APIFriend
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                RequiredLength = 4,
+                RequireNonLetterOrDigit = false,
                 RequireDigit = true,
                 RequireLowercase = true,
                 RequireUppercase = true,
